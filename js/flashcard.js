d5f74826a0a5ed24cb6bd3354de8483de1db4e6e
Tal Haberer and jonathan sweeney <student@epicodus.com>
2016-01-11 15:51:40 -0800

    Initial commit

---
+++ js/flashcard.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+  $(".java").click(function() {
+    $(".java .initially-hidden").slideToggle();
+  });
+
+  $(".modulo").click(function() {
+    $(".modulo .initially-hidden").slideToggle();
+  });
+
+  $(".operators").click(function() {
+    $(".operators .initially-hidden").slideToggle();
+  });
+
+  $(".nan").click(function() {
+    $(".nan .initially-hidden").slideToggle();
+  });
+
+  $(".variables").click(function() {
+    $(".variables .initially-hidden").slideToggle();
+  });
+
+  $(".addition").click(function() {
+    $(".addition .initially-hidden").slideToggle();
+  });
+
+  $(".functions").click(function() {
+    $(".functions .initially-hidden").slideToggle();
+  });
+
+  $(".method").click(function() {
+    $(".method .initially-hidden").slideToggle();
+  });
+
+  $(".arguments").click(function() {
+    $(".arguments .initially-hidden").slideToggle();
+  });
+
+  $(".strings").click(function() {
+    $(".strings .initially-hidden").slideToggle();
+  });
+
+  $(".chain-methods").click(function() {
+    $(".chain-methods .initially-hidden").slideToggle();
+  });
+
+  $(".escape").click(function() {
+    $(".escape .initially-hidden").slideToggle();
+  });
+
+  $(".alert").click(function() {
+    $(".alert .initially-hidden").slideToggle();
+  });
+
+  $(".comment-code").click(function() {
+    $(".comment-code .initially-hidden").slideToggle();
+  });
+
+  $(".booleans").click(function() {
+    $(".booleans .initially-hidden").slideToggle();
+  });
+
+  $(".undefined").click(function() {
+    $(".undefined .initially-hidden").slideToggle();
+  });
+
+  $(".Parameter").click(function() {
+    $(".Parameter .initially-hidden").slideToggle();
+  });
+
+  $(".returns").click(function() {
+    $(".returns .initially-hidden").slideToggle();
+  });
+
+});
